a4d3d468e891196d00efbb750b207cc1384fb414
marphco <5993166+marphco@users.noreply.github.com>
2024-02-18 18:50:18 -0500

    added questions to the index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// Prompt the user for project details
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: Object.keys(licenses),
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
